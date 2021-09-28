3c0ef9cee25fe3052ae21026337e69c597af5e6a
lgu3877 <lgu3877@naver.com>
2021-09-28 16:37:31 +0900

    feat: r balanceGame Question Daily function 2021-09-28 16:37

---
+++ routes/api/balanceGame/selectBalanceGameQuestionDaily.js
@@ -0,0 +1,76 @@
+/**
+ * Created by gunucklee on 2021. 09. 27.
+ *
+ * @swagger
+ * /api/private/balanceGame/question/daily:
+ *   get:
+ *     summary:
+ *     tags: [BalanceGame]
+ *     description: |
+ *       path : /api/private/balanceGame/question/daily
+ *
+ *       * 일일 질문
+ *
+ *     responses:
+ *       200:
+ *         description: 결과 정보
+ *       400:
+ *         description: 에러 코드 400
+ *         schema:
+ *           $ref: '#/definitions/Error'
+ */
+
+const paramUtil = require('../../../common/utils/paramUtil');
+const fileUtil = require('../../../common/utils/fileUtil');
+const mysqlUtil = require('../../../common/utils/mysqlUtil');
+const sendUtil = require('../../../common/utils/sendUtil');
+const errUtil = require('../../../common/utils/errUtil');
+const logUtil = require('../../../common/utils/logUtil');
+
+let file_name = fileUtil.name(__filename);
+
+module.exports = function (req, res) {
+    const _funcName = arguments.callee.name;
+
+    try {
+        req.file_name = file_name;
+        logUtil.printUrlLog(req, `== function start ==================================`);
+        req.paramBody = paramUtil.parse(req);
+
+        checkParam(req);
+
+        mysqlUtil.connectPool(async function (db_connection) {
+            req.innerBody = {};
+
+            req.innerBody['item'] = await querySelect(req, db_connection);
+
+
+            deleteBody(req)
+            sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
+
+        }, function (err) {
+            sendUtil.sendErrorPacket(req, res, err);
+        });
+
+    } catch (e) {
+        let _err = errUtil.get(e);
+        sendUtil.sendErrorPacket(req, res, _err);
+    }
+}
+
+function checkParam(req) {
+}
+
+function deleteBody(req) {
+}
+
+function querySelect(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.querySingle(db_connection
+        , 'call proc_select_balanceGame_question_daily'
+        , [
+            req.headers['user_uid']
+        ]
+    );
+}
