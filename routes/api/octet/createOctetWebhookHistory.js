67bab683929a692e31122f6df21c6562e4a0a6e3
lgu3877 <lgu3877@naver.com>
2021-12-06 17:43:32 +0900

    chore: createOctetWebhookHistory.js already Use function add
    2021-12-06 17:43

--- routes/api/octet/createOctetWebhookHistory.js
+++ routes/api/octet/createOctetWebhookHistory.js
@@ -7,6 +7,7 @@ const mysqlUtil = require('../../../common/utils/mysqlUtil');
 const sendUtil = require('../../../common/utils/sendUtil');
 const errUtil = require('../../../common/utils/errUtil');
 const logUtil = require('../../../common/utils/logUtil');
+const errCode = require("../../../common/define/errCode");
 
 let file_name = fileUtil.name(__filename);
 
@@ -23,9 +24,15 @@ module.exports = function (req, res) {
             req.innerBody = {};
 
             if(req.paramBody['id']) {
+                 let check = await querySelectDepositHistoryCheck(req, db_connection);
+                 paramUtil.checkParam_alreadyUse(check,'이미 해당 기록이 등록되어 있습니다.');
+
                 await queryCreateDepositHistory(req, db_connection);
             }
             else {
+                let check = await querySelectWithdrawHistoryCheck(req, db_connection);
+                paramUtil.checkParam_alreadyUse(check,'이미 해당 기록이 등록되어 있습니다.');
+
                 await queryCreateWithdrawHistory(req, db_connection);
             }
 
@@ -50,6 +57,22 @@ function checkParam(req) {
 function deleteBody(req) {
 }
 
+function querySelectDepositHistoryCheck(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.querySingle(db_connection
+        , 'call proc_select_octet_webhook_deposit_history_check'
+        , [
+              req.paramBody['id']
+            , req.paramBody['coinSymbol']
+            , req.paramBody['fromAddress']
+            , req.paramBody['toAddress']
+            , req.paramBody['amount']
+            , req.paramBody['txid']
+        ]
+    );
+}
+
 function queryCreateDepositHistory(req, db_connection) {
     const _funcName = arguments.callee.name;
 
@@ -62,6 +85,28 @@ function queryCreateDepositHistory(req, db_connection) {
             , req.paramBody['toAddress']
             , req.paramBody['amount']
             , req.paramBody['txid']
+        ]
+    );
+}
+
+
+function querySelectWithdrawHistoryCheck(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.querySingle(db_connection
+        , 'call proc_select_octet_webhook_withdraw_history_check'
+        , [
+              req.paramBody['idx']
+            , req.paramBody['requestId']
+            , req.paramBody['type']
+            , req.paramBody['status']
+            , req.paramBody['coinSymbol']
+            , req.paramBody['txid']
+            , req.paramBody['fromAddress']
+            , req.paramBody['toAddress']
+            , req.paramBody['amount']
+            , req.paramBody['actualFee']
+            , req.paramBody['webhookStatus']
         ]
     );
 }
