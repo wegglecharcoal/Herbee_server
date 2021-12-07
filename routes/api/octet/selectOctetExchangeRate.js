658b5ad0252ec0c98b36500618e4a6385dc90a58
lgu3877 <lgu3877@naver.com>
2021-12-07 10:52:26 +0900

    feat: beecoin and honey exchange rate && me walletaddress value
    2021-12-07 10:52

---
+++ routes/api/octet/selectOctetExchangeRate.js
@@ -0,0 +1,66 @@
+/**
+ * Created by gunucklee on 2021. 12. 07.
+ *
+ * @swagger
+ * /api/private/octet/exchange/rate:
+ *   get:
+ *     summary: 꿀 교환 <-> BEE coin 환율
+ *     tags: [Octet]
+ *     description: |
+ *       path : /api/private/octet/exchange/rate
+ *
+ *       * 꿀 교환 <-> BEE coin 환율
+ *
+ *     responses:
+ *       200:
+ *         description: 결과 정보
+ *       400:
+ *         description: 에러 코드 400
+ *         schema:
+ *           $ref: '#/definitions/Error'
+ */
+
+const paramUtil = require('../../../common/utils/paramUtil');
+const fileUtil = require('../../../common/utils/fileUtil');
+const mysqlUtil = require('../../../common/utils/mysqlUtil');
+const sendUtil = require('../../../common/utils/sendUtil');
+const errUtil = require('../../../common/utils/errUtil');
+const logUtil = require('../../../common/utils/logUtil');
+
+let file_name = fileUtil.name(__filename);
+
+module.exports = function (req, res) {
+    const _funcName = arguments.callee.name;
+
+    try {
+        req.file_name = file_name;
+        logUtil.printUrlLog(req, `== function start ==================================`);
+        req.paramBody = paramUtil.parse(req);
+
+        checkParam(req);
+
+        mysqlUtil.connectPool(async function (db_connection) {
+            req.innerBody = {};
+
+            req.innerBody['item'] = {
+                "exchange_rate" : "1:10"
+            }
+
+            deleteBody(req);
+            sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
+
+        }, function (err) {
+            sendUtil.sendErrorPacket(req, res, err);
+        });
+
+    } catch (e) {
+        let _err = errUtil.get(e);
+        sendUtil.sendErrorPacket(req, res, _err);
+    }
+}
+
+function checkParam(req) {
+}
+
+function deleteBody(req) {
+}
---
+++ routes/api/octet/selectOctetWalletAddress.js
@@ -0,0 +1,76 @@
+/**
+ * Created by gunucklee on 2021. 12. 02.
+ *
+ * @swagger
+ * /api/private/octet/walletAddress:
+ *   get:
+ *     summary: 옥텟 내 지갑 주소 가져오기
+ *     tags: [Octet]
+ *     description: |
+ *       path : /api/private/octet/walletAddress
+ *
+ *       * 옥텟 내 지갑 주소 가져오기
+ *
+ *     responses:
+ *       200:
+ *         description: 결과 정보
+ *       400:
+ *         description: 에러 코드 400
+ *         schema:
+ *           $ref: '#/definitions/Error'
+ */
+
+const paramUtil = require('../../../common/utils/paramUtil');
+const fileUtil = require('../../../common/utils/fileUtil');
+const mysqlUtil = require('../../../common/utils/mysqlUtil');
+const sendUtil = require('../../../common/utils/sendUtil');
+const errUtil = require('../../../common/utils/errUtil');
+const logUtil = require('../../../common/utils/logUtil');
+
+let file_name = fileUtil.name(__filename);
+
+module.exports = function (req, res) {
+    const _funcName = arguments.callee.name;
+
+    try {
+        req.file_name = file_name;
+        logUtil.printUrlLog(req, `== function start ==================================`);
+        req.paramBody = paramUtil.parse(req);
+
+        checkParam(req);
+
+        mysqlUtil.connectPool(async function (db_connection) {
+            req.innerBody = {};
+
+            req.innerBody['item'] = await querySelect(req, db_connection);
+
+            deleteBody(req);
+            sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
+
+        }, function (err) {
+            sendUtil.sendErrorPacket(req, res, err);
+        });
+
+    } catch (e) {
+        let _err = errUtil.get(e);
+        sendUtil.sendErrorPacket(req, res, _err);
+    }
+}
+
+function checkParam(req) {
+}
+
+function deleteBody(req) {
+}
+
+function querySelect(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.querySingle(db_connection
+        , 'call proc_select_octet_walletAddress'
+        , [
+            req.headers['user_uid']
+        ]
+    );
+}
+
