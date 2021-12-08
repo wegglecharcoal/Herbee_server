ce8ddfc5f20df4c65d3b0f20d73716232312344c
lgu3877 <lgu3877@naver.com>
2021-12-08 17:47:26 +0900

    chore: channel editing
    2021-12-08 17:47

--- common/utils/fcmUtil.js
+++ common/utils/fcmUtil.js
@@ -19,7 +19,7 @@ module.exports = {
               item['fcm_push_token_list']
             , "메시지 알림"
             , `${item['fcm_nickname']}님이 메시지를 보냈습니다.`
-            , "메시지 알림"
+            , "메시지"
             , "0"
             , item['fcm_filename']
         );
@@ -30,7 +30,7 @@ module.exports = {
               [item['fcm_push_token']]
             , "팔로우 알림"
             , `${item['fcm_nickname']}님이 당신을 팔로우 했습니다.`
-            , "팔로우 알림"
+            , "팔로우"
             , "1"
             , item['fcm_filename']
         );
@@ -41,7 +41,7 @@ module.exports = {
               [item['fcm_push_token']]
             , "게시물 좋아요 알림"
             , `${item['fcm_nickname']}님이 게시물에 좋아요를 눌렀습니다.`
-            , "좋아요 알림"
+            , "좋아요"
             , "2"
             , item['fcm_filename']
         );
@@ -52,7 +52,7 @@ module.exports = {
               [item['fcm_push_token']]
             , "댓글 좋아요 알림"
             , `${item['fcm_nickname']}님이 댓글에 좋아요를 눌렀습니다.`
-            , "좋아요 알림"
+            , "좋아요"
             , "2"
             , item['fcm_filename']
         );
@@ -62,7 +62,7 @@ module.exports = {
               [item['fcm_push_token']]
             , "댓글 알림"
             , `${item['fcm_nickname']}님이 게시물에 댓글을 남겼습니다.`
-            , "댓글 알림"
+            , "댓글"
             , "3"
             , item['fcm_filename']
         );
@@ -72,7 +72,7 @@ module.exports = {
               item['fcm_push_token_list']
             , "약속 생성 알림"
             , `${item['fcm_nickname']}님이 약속을 잡았습니다.`
-            , "약속 알림"
+            , "약속"
             , "4"
             , item['fcm_filename']
         );
@@ -82,7 +82,7 @@ module.exports = {
               [item['fcm_push_token']]
             , "약속 수락 알림"
             , `${item['fcm_nickname']}님이 약속을 수락했습니다.`
-            , "약속 알림"
+            , "약속"
             , "4"
             , item['fcm_filename']
         );
@@ -92,7 +92,7 @@ module.exports = {
               [item['push_token']]
             , "약속 한 시간 전 알림"
             , `${item['nickname']}님과의 약속 잊지 않으셨죠? 출발할 때 알려주세요.`
-            , "약속 알림"
+            , "약속"
             , "4"
             , item['filename']
         );
@@ -102,7 +102,7 @@ module.exports = {
             [item['push_token']]
             , "약속 한 시간 전 알림"
             , `${item['nickname']}님이 약속 장소로 향하고 있습니다.`
-            , "약속 알림"
+            , "약속"
             , "4"
             , item['filename']
         );
@@ -112,7 +112,7 @@ module.exports = {
               [item['push_token']]
             , "약속 출발"
             , `${item['nickname']}님과의 약속 어떠셨나요?`
-            , "약속 알림"
+            , "약속"
             , "4"
             , item['filename']
         );
