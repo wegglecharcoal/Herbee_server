4d6f41a7f00b3e796afef398a68f8f53b7c45e04
MoneyQuestTech <moneyquesttech@gmail.com>
2021-12-14 17:46:22 +0900

    chore: 메인 옥텟 적용
    2021-12-14 17:31

---
+++ routes/api/user/test.js
@@ -0,0 +1,131 @@
+/**
+ * Created by gunucklee on 2021. 12. 14.
+ *
+ * @swagger
+ * /api/private/octet/test:
+ *   get:
+ *     summary: test
+ *     tags: [Octet]
+ *     description: |
+ *       path : /api/private/octet/test
+ *
+ *       * test
+ *
+ *     responses:
+ *       200:
+ *         description: 결과 정보
+ *       400:
+ *         description: 에러 코드 400
+ *         schema:
+ *           $ref: '#/definitions/Error'
+ */
+const paramUtil = require('../../../common/utils/paramUtil');
+const fileUtil = require('../../../common/utils/fileUtil');
+const mysqlUtil = require('../../../common/utils/mysqlUtil');
+const sendUtil = require('../../../common/utils/sendUtil');
+const errUtil = require('../../../common/utils/errUtil');
+const logUtil = require('../../../common/utils/logUtil');
+const jwtUtil = require('../../../common/utils/jwtUtil');
+const octetUtil = require('../../../common/utils/octetUtil');
+
+const errCode = require('../../../common/define/errCode');
+
+let file_name = fileUtil.name(__filename);
+
+module.exports = function (req, res) {
+    const _funcName = arguments.callee.name;
+
+    try {
+        req.file_name = file_name;
+
+        req.paramBody = paramUtil.parse(req);
+
+        logUtil.printUrlLog(req, `header: ${JSON.stringify(req.headers)}`);
+
+        mysqlUtil.connectPool(async function (db_connection) {
+            req.innerBody = {};
+
+            req.innerBody['item'] = await query(req, db_connection);
+
+            for (let idx in req.innerBody['item']) {
+                await octetFunction(req.innerBody['item'][idx], db_connection);
+            }
+
+
+            sendUtil.sendSuccessPacket(req, res, req.innerBody, true);
+
+        }, function (err) {
+            sendUtil.sendErrorPacket(req, res, err);
+        } );
+
+    }
+    catch (e) {
+        let _err = errUtil.get(e);
+        sendUtil.sendErrorPacket(req, res, _err);
+    }
+}
+
+
+function query(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.queryArray(db_connection
+        , 'call proc_select_test'
+        , []
+    );
+}
+
+function querySelectOctetAccessToken(req, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.querySingle(db_connection
+        , 'call proc_select_octet_access_token'
+        , []
+    );
+}
+
+
+function queryUpdateOctetAccessToken(accessToken, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.querySingle(db_connection
+        , 'call proc_update_octet_access_token'
+        , [
+            accessToken
+        ]
+    );
+}
+
+
+
+function queryUpdateWalletAddress(wallet_address, db_connection) {
+    const _funcName = arguments.callee.name;
+
+    return mysqlUtil.querySingle(db_connection
+        , 'call proc_update_wallet_address'
+        , [
+              wallet_address['data']['uid']
+            , wallet_address['data']['addresses'][0]['address']
+        ]
+    );
+}
+
+
+async function octetFunction(test, db_connection) {
+
+    let current_access_token = await querySelectOctetAccessToken(test, db_connection);
+
+    let get_token_result = await octetUtil.octetToken(current_access_token['access_token']);
+
+    if( get_token_result !== 'maintain'
+        && get_token_result !== null && get_token_result !== undefined ) {
+        await queryUpdateOctetAccessToken(get_token_result, db_connection);
+    }
+
+    let wallet_address = await octetUtil.octetCreateAddress(test['uid'],
+        get_token_result === 'maintain' ? current_access_token['access_token'] : get_token_result);
+    wallet_address['data']['uid'] = test['uid'];
+
+    await queryUpdateWalletAddress(wallet_address, db_connection);
+
+}
