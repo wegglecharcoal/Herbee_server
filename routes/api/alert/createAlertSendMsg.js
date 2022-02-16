25767ef7d8d0d464badd26f32e0898a66e12ccec
weggleorange <weggle_orange@weggle.io>
2022-02-16 11:14:14 +0900

    chore: fcm update 2022-02-15 18:55

--- routes/api/alert/createAlertSendMsg.js
+++ routes/api/alert/createAlertSendMsg.js
@@ -127,13 +127,14 @@ async function fcmFunction(req, db_connection) {
     req.innerBody['fcm_target_uid'] = req.paramBody['chat_room_uid'];
 
     for (let idx in herbee_language_list) {
-        console.log('asdfsafsdfsfs: ' + herbee_language_list['idx'] );
+        console.log('asdfsafsdfsfs: ' + herbee_language_list[idx] );
         req.innerBody['fcm_push_token_other_list'] = [];
 
         for(let idx in chatRoomUserList) {
             console.log('123121111: ' + herbee_language_list[idx]);
             console.log('123121111: ' + chatRoomUserList[idx]['fcm_language_other']);
-            console.log('wdasdasasda:' + herbee_language_list[idx] == chatRoomUserList[idx]['fcm_language_other']);
+            console.log('wdasdasasda:' + String(herbee_language_list[idx]) === chatRoomUserList[idx]['fcm_language_other']);
+            console.log('wdasdasasda:' + String(herbee_language_list[idx]) == chatRoomUserList[idx]['fcm_language_other']);
 
             if(herbee_language_list[idx] == chatRoomUserList[idx]['fcm_language_other']) {
                 req.innerBody['fcm_push_token_other_list'].push(chatRoomUserList[idx]['fcm_push_token_other']);
