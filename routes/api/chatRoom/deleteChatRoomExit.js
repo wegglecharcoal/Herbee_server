f2a19fad1b8187e38381647fbda22e0f6a992941
unow30 <https://github.com/unow30>
2022-05-26 15:05:21 +0900

    fix: deleteChatRoomExit error fix 2022-05-26 15:05

--- routes/api/chatRoom/deleteChatRoomExit.js
+++ routes/api/chatRoom/deleteChatRoomExit.js
@@ -51,13 +51,14 @@ module.exports = function (req, res) {
             req.innerBody = {};
 
             req.innerBody['item'] = await queryCheckUser(req, db_connection);
+            // console.log( req.innerBody['item'])
             if (!req.innerBody['item']) {
                 errUtil.createCall(errCode.non_participating_chatRoom, `Error code: 451 [참여하지 않은 채팅방입니다.]`);
                 return;
             }
 
             req.innerBody['item'] = await queryCheckIsHead(req, db_connection);
-
+            // console.log( req.innerBody['item'])
             if (req.innerBody['item']['result'] == 0 ) {
                 errUtil.createCall(errCode.fail_exit_chatRoom, `Error code: 202 [모임 채팅방은 혼자 있을 경우에만 채팅방에서 나갈 수 있습니다.]`);
                 return;
@@ -111,7 +112,7 @@ function queryCheckIsHead(req, db_connection) {
         , [
               req.headers['user_uid']
             , req.paramBody['chat_room_uid']
-            , 1    // is_head      {0: false, 1: true}
+            , 0    // is_head      {0: false, 1: true}
         ]
     );
 }
